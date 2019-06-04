6a70f5e36b3dc9be3be90727be048e60a3a9ec69
Brian Salemi <briandsalemi@gmail.com>
2019-06-04 15:42:23 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -11,7 +11,7 @@ class Student
 
   def self.create_from_collection(students_array)
     students_array.each do |student|
-    student = self.new(student_hash)
+    student = self.new
   end
   end
 
